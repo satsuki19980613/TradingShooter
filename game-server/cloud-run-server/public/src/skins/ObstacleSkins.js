e77eff81adcd71a7f42c2d7fb3d3983b6b9bd1f0
satsuki19980613 <wsk641@gmail.com>
2025-11-24 17:38:30 +0900

    更新

--- game-server/cloud-run-server/public/src/skins/ObstacleSkins.js
+++ game-server/cloud-run-server/public/src/skins/ObstacleSkins.js
@@ -1,26 +1,29 @@
 import { HexagonFortressSkin } from "./obstacles/HexagonFortress.js";
+// ▼▼▼ 新しいスキンのインポート ▼▼▼よふかしうた
 import { LongCornerMagentaSkin } from "./obstacles/LongCornerMagenta.js";
 import { WideNeonSkin } from "./obstacles/WideNeon.js";
 import { UShapeHangarSkin } from "./obstacles/UShapeHangar.js";
 import { LongCrossSkin } from "./obstacles/LongCross.js";
 import { StandardCyberSkin } from "./obstacles/StandardCyber.js";
+// ▲▲▲
 
 export const ObstacleSkins = {
-
-  // ▼ 追加: IDと関数の紐付け (progressを渡すラッパー関数)
+  // 既存のアニメーション要塞
   "obs-hexagon-fortress-animated": (progress) => HexagonFortressSkin(progress, "#00ffea"),
+
+  // ▼▼▼ 新しいスキンの登録 ▼▼▼
   "obs-long-corner": (progress) => LongCornerMagentaSkin(progress),
   "obs-wide-neon": (progress) => WideNeonSkin(progress),
   "obs-ushape-hangar": (progress) => UShapeHangarSkin(progress),
   "obs-long-cross": (progress) => LongCrossSkin(progress),
   "obs-standard-cyber": (progress) => StandardCyberSkin(progress),
+  // ▲▲▲
 
   "default": (ctx, w, h) => {
     ctx.fillStyle = "#333";
     ctx.fillRect(0, 0, w, h);
     ctx.strokeStyle = "#fff";
     ctx.strokeRect(0, 0, w, h);
-    // ↓ これが表示されているはずです
     ctx.fillStyle = "white";
     ctx.font = "12px Arial";
     ctx.fillText("NoSkin", 2, 12);
