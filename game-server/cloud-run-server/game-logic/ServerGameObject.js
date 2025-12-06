3ddb75afc2b60f625b89d1ff0368361898027f6b
satsuki19980613 <wsk641@gmail.com>
2025-12-06 10:30:31 +0900

    更新

--- game-server/cloud-run-server/game-logic/ServerGameObject.js
+++ game-server/cloud-run-server/game-logic/ServerGameObject.js
@@ -1,8 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 
-/**
- * すべてのサーバー側ゲームオブジェクトの基底クラス (描画ロジックなし)
- */
+
 export class ServerGameObject {
   constructor(x, y, radius, type) {
     this.id = uuidv4();
